3d6ba935631f7b3f11e7386dbeacccad37976d6c
AntonioDuarte06 <antoniord@outlook.com>
2021-05-23 15:18:20 -0300

    Criando entidade de Usuario e suas camadas

---
+++ src/main/java/com/duarte/mycash/domain/UsuarioRole.java
@@ -0,0 +1,7 @@
+package com.duarte.mycash.domain;
+
+public enum UsuarioRole {
+
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ src/main/java/com/duarte/mycash/service/UsuarioService.java
@@ -0,0 +1,70 @@
+package com.duarte.mycash.service;
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.duarte.mycash.domain.Usuario;
+import com.duarte.mycash.domain.UsuarioRole;
+import com.duarte.mycash.exception.UsuarioException;
+import com.duarte.mycash.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repo;
+
+    public void registraUsuarioAdmin(String email, String senha) {
+
+        if (repo.findByEmail(email).isEmpty()) {
+
+            Usuario usuario = new Usuario();
+            usuario.setEmail(email);
+            usuario.setSenha(senha);
+            usuario.setRole(UsuarioRole.ROLE_ADMIN);
+
+            repo.save(usuario);
+        }
+    }
+
+    public List<Usuario> buscaTodos() {
+
+        return repo.findAll();
+    }
+
+    public Usuario buscaPorId(Integer id) {
+        return repo.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    }
+
+    public Usuario save(String email, String senha) {
+
+        if (repo.findByEmail(email).isPresent())
+            throw new UsuarioException("Já existe um usuario com esse email");
+
+        Usuario usuario = new Usuario();
+
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+        usuario.setRole(UsuarioRole.ROLE_USER);
+
+        return repo.save(usuario);
+    }
+
+    public Usuario findByEmail(String email) {
+
+        return repo.findByEmail(email)
+                .orElseThrow(() -> new UsuarioException("Usuário não encontrado."));
+    }
+
+    public Usuario resetarSenha(String email, String senha) {
+        Usuario usuario = findByEmail(email);
+        usuario.setSenha(senha);
+
+        return repo.save(usuario);
+    }
+
+}
